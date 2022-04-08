6dd857022959c7a9fe28ea591626a9b69e7242b4
Eric waweru <ericwaweru68@gmail.com>
2022-04-08 15:20:52 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.Gender = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAddrss = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.txtOcc = new System.Windows.Forms.TextBox();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Operations = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Operations.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rdbFemale);
+            this.groupBox1.Controls.Add(this.Gender);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.txtAddrss);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtFname);
+            this.groupBox1.Controls.Add(this.txtOcc);
+            this.groupBox1.Controls.Add(this.rdbMale);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(694, 495);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registration";
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(161, 222);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(74, 20);
+            this.rdbFemale.TabIndex = 16;
+            this.rdbFemale.TabStop = true;
+            this.rdbFemale.Text = "Female";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Location = new System.Drawing.Point(11, 226);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(52, 16);
+            this.Gender.TabIndex = 15;
+            this.Gender.Text = "Gender";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 336);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(657, 183);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // txtAddrss
+            // 
+            this.txtAddrss.Location = new System.Drawing.Point(410, 170);
+            this.txtAddrss.Name = "txtAddrss";
+            this.txtAddrss.Size = new System.Drawing.Size(223, 22);
+            this.txtAddrss.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(77, 176);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(223, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(410, 106);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(223, 22);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(410, 41);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(223, 22);
+            this.txtFname.TabIndex = 10;
+            // 
+            // txtOcc
+            // 
+            this.txtOcc.Location = new System.Drawing.Point(87, 106);
+            this.txtOcc.Name = "txtOcc";
+            this.txtOcc.Size = new System.Drawing.Size(223, 22);
+            this.txtOcc.TabIndex = 9;
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(87, 222);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(58, 20);
+            this.rdbMale.TabIndex = 7;
+            this.rdbMale.TabStop = true;
+            this.rdbMale.Text = "Male";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 35);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(223, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(341, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Occupation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // txtID
+            // 
+            this.txtID.AutoSize = true;
+            this.txtID.Location = new System.Drawing.Point(12, 176);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(20, 16);
+            this.txtID.TabIndex = 2;
+            this.txtID.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(33, 42);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 61);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Operations
+            // 
+            this.Operations.Controls.Add(this.btnDelete);
+            this.Operations.Controls.Add(this.btnUpdate);
+            this.Operations.Controls.Add(this.btnSave);
+            this.Operations.Location = new System.Drawing.Point(771, 54);
+            this.Operations.Name = "Operations";
+            this.Operations.Size = new System.Drawing.Size(202, 443);
+            this.Operations.TabIndex = 1;
+            this.Operations.TabStop = false;
+            this.Operations.Text = "Operations";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(33, 306);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(137, 63);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(33, 174);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(137, 63);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(771, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Search for record";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(148, 273);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(291, 22);
+            this.txtSearch.TabIndex = 18;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 592);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Operations);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Operations.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAddrss;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox txtOcc;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox Operations;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Registration
+{
+    public partial class Form1 : Form
+
+    {
+        string integrationdb_conn_string = "Server=HM;Database=registration;uid=sa;pwd=Jumpman535;";
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adp;
+        DataTable dt;
+       
+        int ID;
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(integrationdb_conn_string);
+            displaydata();
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;  
+
+        }
+
+       
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text==""||txtFname.Text==""||txtEmail.Text==""||txtID.Text==""||txtOcc.Text==""||txtAddrss.Text==""||Gender.Text=="" )
+            {
+                MessageBox.Show("Please Fill Columns");
+            }
+            else
+            {
+                try
+                {
+
+                    string gender;
+
+                    if (rdbMale.Checked)
+                    {
+                        gender = "Male";
+                    }
+                    else
+                    {
+                        gender = "Female";
+                    }
+                    con.Open();
+                    cmd = new SqlCommand("insert into Employee(Employee_Name,Employee_FName,Employees_Occupation,Employee_Email,Emp_ID,Gender,Addrss) values ( '" + txtName.Text + " ', '" + txtFname.Text + " ', '" + txtOcc.Text + " ',  '" + txtEmail.Text + " ','" + txtID.Text + " ',  '" + gender + " ', '" + txtAddrss.Text+  " ') ", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    
+                    MessageBox.Show("Employee saved in Database");
+                    clear();
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public void clear()
+        {
+            txtName.Text = "";
+            txtFname.Text = "";
+            txtOcc.Text = "";
+            textBox5.Text = "";
+            txtEmail.Text = "";
+            txtAddrss.Text = "";
+        }
+
+      public void displaydata()
+        {
+            try
+            {
+
+                dt = new DataTable();
+                con.Open();
+                adp = new SqlDataAdapter("select * from Employee", con);
+                adp.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+                
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ID = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtFname.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtEmail.Text= dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtOcc.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();  
+            
+
+            rdbFemale.Checked = false;
+            rdbMale.Checked = true;
+
+            if (dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString()=="Female")
+            {
+                rdbFemale.Checked=true;    
+                rdbMale.Checked=false;   
+
+            }
+            txtAddrss.Text = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
+
+            btnDelete.Enabled = true;
+            btnUpdate.Enabled = true;
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gender;
+
+                if (rdbFemale.Checked)
+                {
+                    gender = "Female";
+
+                }
+                else
+                {
+                    gender = "Male";
+                }
+
+                con.Open();
+                cmd = new SqlCommand("update Employee set Employee_Name= '" + txtName.Text + " ',Employee_FName='" + txtFname.Text + " ', Employees_Occupation='" + txtOcc.Text + " ',Employee_Email='" + txtEmail.Text + " ',Emp_ID='" + txtID.Text + " ',Gender='" +gender + " ',Addrss='" + txtAddrss.Text + " ' where Employee_ID = '" +ID + " '", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                displaydata();
+                MessageBox.Show("Your Data has been updated");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("delete from Employee where Employee_Id= '" + ID + " ' ",con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record has been deleted");
+                displaydata();  
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            adp = new SqlDataAdapter("select * from Employee where Employee_Name like '%" + txtSearch.Text+ "%'", con);
+            dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource= dt;   
+            con.Close();
+
+        }
+    }
+}
+
+
+
+
